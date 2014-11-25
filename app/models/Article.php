ee41444fc033ed2c2a52eab0747680754db51737
Rory van Heerden <rory@eishgaming.co.za>
2014-11-26 01:10:34 +0200

    Added relationship to ArticleRevisions in the Article model

--- app/models/Article.php
+++ app/models/Article.php
@@ -8,7 +8,27 @@ class Article extends BaseModel {
   ];
   // Don't forget to fill this array
   protected $guarded = [
-      'id'
+      'id',
   ];
-
+  
+  public function revisions()
+  {
+    return $this->hasMany('ArticleRevision', 'article_id');
+  }
+  
+  /**
+   * Returns the current article revision
+   */
+  public function headRevision()
+  {
+    return $this->revisions()->where('status', '=', 'HEAD')->first();
+  }
+  
+  /**
+   * Returns all article revisions except the head/current one
+   */
+  public function previousRevisions()
+  {
+    return $this->revisions()->where('status', '=', 'REVISED');
+  }
 }
